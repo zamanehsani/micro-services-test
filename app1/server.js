511f895d6c1c79647fd701f29d20c029519a4a66
Zaman Ehsani <zamanehsani@gmail.com>
2024-12-23 18:21:31 +0400

    added the kubernetes infr filder and updated the Docker files with the codes in app1

--- app1/server.js
+++ app1/server.js
@@ -1,20 +1,23 @@
 import express from "express";
-import { setupRabbitMQ, publishEvent, consumeEvents } from "./index.js";
+// import { setupRabbitMQ, publishEvent, consumeEvents } from "./index.js";
 
 const app = express();
 app.use(express.json());
 
-const queue = "events";
+// const queue = "events";
 
 (async () => {
-  const channel = await setupRabbitMQ();
-  consumeEvents(channel, queue);
+  // const channel = await setupRabbitMQ();
+  // consumeEvents(channel, queue);
 
   app.post("/publish", async (req, res) => {
     const { message } = req.body;
-    await publishEvent(channel, queue, message);
+    // await publishEvent(channel, queue, message);
     res.json({ status: "Message published by app1" });
   });
 
-  app.listen(3001, () => console.log("App1 listening on port 3001"));
+  app.listen(3001, () => {
+    console.log("App1 connected to RabbitMQ v55");
+    console.log("App1 listening on port 3001");
+  });
 })();
